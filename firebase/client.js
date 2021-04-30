c9f4e2aee95ed0a4812a2a0405c54a8133fb194b
eosfelipe <escobedo.felipe@hotmail.com>
2021-04-30 16:31:20 -0500

    add lint and prettier

--- firebase/client.js
+++ firebase/client.js
@@ -1,35 +1,35 @@
-import firebase from 'firebase';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC7p6jPHpYuZ2lo3LLtCzrVccDtlGDBbhU",
-    authDomain: "nextjs-devtter.firebaseapp.com",
-    projectId: "nextjs-devtter",
-    storageBucket: "nextjs-devtter.appspot.com",
-    messagingSenderId: "17815410765",
-    appId: "1:17815410765:web:52a5e08b58ea6216f4a404"
+  apiKey: "AIzaSyC7p6jPHpYuZ2lo3LLtCzrVccDtlGDBbhU",
+  authDomain: "nextjs-devtter.firebaseapp.com",
+  projectId: "nextjs-devtter",
+  storageBucket: "nextjs-devtter.appspot.com",
+  messagingSenderId: "17815410765",
+  appId: "1:17815410765:web:52a5e08b58ea6216f4a404",
+};
+
+!firebase.apps.length && firebase.initializeApp(firebaseConfig);
+
+const mapUserFromFirebaseAuth = (user) => {
+  const { displayName, email, photoURL } = user;
+  return {
+    avatar: photoURL,
+    username: displayName,
+    email,
   };
+};
 
-  !firebase.apps.length && firebase.initializeApp(firebaseConfig)
+export const onAuthStateChanged = (onChange) => {
+  return firebase.auth().onAuthStateChanged((user) => {
+    const normalizeUser = mapUserFromFirebaseAuth(user);
+    onChange(normalizeUser);
+  });
+};
 
-  const mapUserFromFirebaseAuth = (user) => {
-    const {displayName, email, photoURL} = user
-    return {
-        avatar: photoURL,
-        username: displayName,
-        email
-    }
-  }
-
-  export const onAuthStateChanged = (onChange) => {
-      return firebase.auth().onAuthStateChanged(user => {
-          const normalizeUser = mapUserFromFirebaseAuth(user)
-          onChange(normalizeUser)
-      })
-  }
-
-  export const loginWithGithub = () => {
-      const githubProvider = new firebase.auth.GithubAuthProvider()
-      return firebase
-      .auth()
-      .signInWithPopup(githubProvider)
-  }
+export const loginWithGithub = () => {
+  const githubProvider = new firebase.auth.GithubAuthProvider();
+  return firebase.auth().signInWithPopup(githubProvider);
+};
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,28 @@
-import Head from 'next/head'
-import { useEffect, useState } from 'react'
-import AppLayout from '../components/AppLayout'
-import Button from '../components/Button'
-import Github from '../components/Icons/Github'
-import { colors } from '../styles/theme'
-import {loginWithGithub, onAuthStateChanged} from '../firebase/client'
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import AppLayout from "../components/AppLayout";
+import Button from "../components/Button";
+import Github from "../components/Icons/Github";
+import { colors } from "../styles/theme";
+import { loginWithGithub, onAuthStateChanged } from "../firebase/client";
 
 export default function Home() {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    onAuthStateChanged(setUser)
-  }, [])
+    onAuthStateChanged(setUser);
+  }, []);
 
   const handleLogin = () => {
-    loginWithGithub().then(user => {
-      setUser(user)
-      console.log(user)
-    }).catch(error => {
-      console.log(error)
-    }) 
-    
-  }
+    loginWithGithub()
+      .then((user) => {
+        setUser(user);
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
     <>
@@ -31,48 +32,49 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <AppLayout>
-      <section>
-        <img src="/devter-logo.png" alt="Logo" />
-        <h1>Devtter</h1>
-        <h2>Talk about development with developers 🧑👧</h2>
-        <div>
-          {
-            user === null ? (<Button onClick={handleLogin}>
-              <Github fill={colors.white} width={24} height={24} />
-              Login with Github</Button>) : (
-                <div>
-                  <img src={user.avatar} alt={user.username} />
-                  <strong>{user.username}</strong>
-                </div>
-              )
-          }
-        </div>
-      </section>
+        <section>
+          <img src="/devter-logo.png" alt="Logo" />
+          <h1>Devtter</h1>
+          <h2>Talk about development with developers 🧑👧</h2>
+          <div>
+            {user === null ? (
+              <Button onClick={handleLogin}>
+                <Github fill={colors.white} width={24} height={24} />
+                Login with Github
+              </Button>
+            ) : (
+              <div>
+                <img src={user.avatar} alt={user.username} />
+                <strong>{user.username}</strong>
+              </div>
+            )}
+          </div>
+        </section>
       </AppLayout>
       <style jsx>{`
-      img {
-        width: 120px;
-      }
-      div {
-        margin-top: 16px;
-      }
-      section {
-        display: grid;
-        height: 100%;
-        place-content: center;
-        place-items: center;
-      }
-      h1 {
-        color: ${colors.primary};
-        font-weight: 800;
-        margin-bottom: 16px;
-      }
-      h2 {
-        color: ${colors.secondary};
-        font-size: 21px;
-        margin: 0;
-      }
+        img {
+          width: 120px;
+        }
+        div {
+          margin-top: 16px;
+        }
+        section {
+          display: grid;
+          height: 100%;
+          place-content: center;
+          place-items: center;
+        }
+        h1 {
+          color: ${colors.primary};
+          font-weight: 800;
+          margin-bottom: 16px;
+        }
+        h2 {
+          color: ${colors.secondary};
+          font-size: 21px;
+          margin: 0;
+        }
       `}</style>
-      </>
-  )
+    </>
+  );
 }
